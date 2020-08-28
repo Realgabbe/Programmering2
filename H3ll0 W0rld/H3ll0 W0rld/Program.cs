0a9af60e34ced6b7b7db590755b6bd008f2a9562
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2020-08-28 14:29:18 +0200

    Initial commit

---
+++ H3ll0 W0rld/H3ll0 W0rld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H3ll0_W0rld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
