5744602d4a78c51711067ab499eccdd30c5ddcc4
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2020-08-30 17:21:29 +0200

    program fördigt

--- H3ll0 W0rld/H3ll0 W0rld/Program.cs
+++ H3ll0 W0rld/H3ll0 W0rld/Program.cs
@@ -6,7 +6,36 @@ namespace H3ll0_W0rld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hejsan einf drogij wepipiwjrg!");
+
+            Console.WriteLine("HEJSAN NIKLAS :)")
+
+            Console.WriteLine("Vänligen fyll i ditt namn.");
+            string namn = Console.ReadLine();
+
+            Console.WriteLine("Vänligen fyll i din ålder.");
+            int ålder = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Är du vid liv? Skriv 1 om du är vid liv och om du inte är vid liv skriv något annat tal.");
+            Boolean levande = true;
+            int svar = Convert.ToInt32(Console.ReadLine());
+            if (svar == 1)
+            {
+                levande = true;
+            }
+            else
+            {
+                levande = false;
+            }
+
+            if (levande == true)
+            {
+                Console.WriteLine("Du är en levande person vars namn är " + namn + ". Du är " + ålder + " år gammal.");
+            }
+            else 
+            {
+                Console.WriteLine("Du är en icke levande person vars namn är " + namn + ". Du är " + ålder + "  år gammal.");
+            }
+
         }
     }
 }
