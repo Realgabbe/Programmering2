65b5e6b6b342f78b9970ae0fbf7a23833680a57b
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2020-09-11 18:29:29 +0200

    Metoder färdiga

---
+++ H3ll0 W0rld/Metoder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 5, 7, 12 };
+            string[] names = { "Gabriel", "Jacob", "Emil", "Theo", "Petter" };
+
+            Console.WriteLine(addition(numbers));
+            namereverse(names);
+            
+            int[] bigandsmall = bigsmall(numbers);
+
+            for (int i = 0; i < bigandsmall.Length; i++)
+            {
+                Console.WriteLine(bigandsmall[i]);
+            }
+        }
+
+        static int addition(int[] numbers)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        static void namereverse(string[] names)
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+
+        static int[] bigsmall(int[] numbers)
+        {
+            int biggest = numbers.Max();
+            int smallest = numbers.Min();
+
+            int[] biggestsmallest = { biggest, smallest };
+
+            return biggestsmallest;
+
+        }
+    }
+}
