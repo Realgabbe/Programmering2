4e23643e29d6bf30ae921402372fbaf418406974
Realgabbe <gabriel.billskog@elev.ga.ntig.se>
2020-11-29 21:59:49 +0100

    initial commit och last commit B)

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,258 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        long number1 = 0; // Ska lagra information tillfälligt för att sedan skickas till displayen
+        long number2 = 0; // Ska lagra information tillfälligt för att sedan skickas till displayen 
+        string operation = ""; // String för att lagra vilken typ av operation som användaren väljer. Operationer är t.ex "+", "-" & "x^2"
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void square_root_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Execute_Click(object sender, RoutedEventArgs e)
+        {
+            switch (operation) // Denna switchen är baserad på vad stringen "operation" har för värde. Om stringen "operation" t.ex har värdet "+" så kommer den att hoppa till + casen
+            {
+                case "+":
+                    result_window.Text = (number1 + number2).ToString();
+                    break;
+
+                case "-":
+                    result_window.Text = (number1 - number2).ToString();
+                    break;
+
+                case "*":
+                    result_window.Text = (number1 + number2).ToString();
+                    break;
+
+                case "/":
+                    result_window.Text = (number1 / number2).ToString();
+                    break;
+
+                case "x^2":
+                    result_window.Text = (number1 * number1).ToString();
+                    break;
+
+                case "squareRoot":
+                    result_window.Text = (Math.Sqrt(number1)).ToString();
+                    break;
+
+                case "numlock":
+                    result_window.Text = "numlock";
+                    break;
+            }
+        }
+
+        private void button_Plus_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+            result_window.Text = "0";
+        }
+
+        private void button_Zero_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10);
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10);
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_One_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 1;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 1;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Two_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 2;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 2;
+                result_window.Text = number2.ToString();
+            }
+        }
+
+        private void button_Three_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 3;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 3;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Minus_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "-";
+            result_window.Text = "0";
+        }
+
+        private void button_Four_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 4;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 4;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Five_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 5;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 5;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Six_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 6;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 6;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "*";
+            result_window.Text = "0";
+        }
+
+        private void button_Seven_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 7;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 7;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Eight_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 8;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 8;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Nine_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 9;
+                result_window.Text = number1.ToString();
+            }
+
+            else
+            {
+                number2 = (number2 * 10) + 9;
+                result_window.Text = number2.ToString();
+            }
+
+        }
+
+        private void button_Division_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "/";
+            result_window.Text = "0";
+        }
+
+        private void button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 0;
+            number2 = 0;
+            operation = "";
+            result_window.Text = "0";
+        }
+
+        private void button_squareRoot_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "squareRoot";
+        }
+
+        private void button_PowerOf_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "x^2";
+        }
+
+        private void button_Numlock_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "numlock";
         }
     }
 }
